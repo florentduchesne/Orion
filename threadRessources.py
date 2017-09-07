bffe66b0e3b6e761dd24baa430b3aef6d45114e5
Duchesne Florent <1526952@decinfo.cvm>
2017-09-07 11:53:15 -0400

    gestion des ressources pour la creation des mines

--- threadRessources.py
+++ threadRessources.py
@@ -18,9 +18,15 @@ class ThreadRessources(threading.Thread):
                 for i in range(self.systemes.__len__()):#boucle a travers les systemes
                     for j in range(self.systemes.__getitem__(i).planetes.__len__()):#boucle a travers les planetes
                         for k in range(self.systemes.__getitem__(i).planetes.__getitem__(j).infrastructures.__len__()):#boucle a travers les infrastructures
+                            self.systemes.__getitem__(i).planetes.__getitem__(j).ressource.humain += 2 #on augmente la population
                             if( isinstance(self.systemes.__getitem__(i).planetes.__getitem__(j).infrastructures.__getitem__(k), Mine)):
                                 print ("un mine!")
+                                if(self.systemes.__getitem__(i).planetes.__getitem__(j).ressourceACollecter.bronze > 0):
+                                    self.systemes.__getitem__(i).planetes.__getitem__(j).ressourceACollecter.bronze -= 5
+                                    self.systemes.__getitem__(i).planetes.__getitem__(j).ressource.bronze += 5
+                                print (self.systemes.__getitem__(i).planetes.__getitem__(j).ressource.bronze)
                             elif(isinstance(self.systemes.__getitem__(i).planetes.__getitem__(j).infrastructures.__getitem__(k), Ville)):
-                                print ("une ville!")
+                                #print ("une ville!")
+                                pass
             except:
-                pass
+                print("erreur")
