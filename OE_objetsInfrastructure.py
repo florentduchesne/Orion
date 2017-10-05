c11bb249c99c0b1cf0f79974ecbb4cd9238197aa
Nguyen VuAndrew <1495573@decinfo.cvm>
2017-10-05 12:21:18 -0400

    Version Andrew 051017 1

--- OE_objetsInfrastructure.py
+++ OE_objetsInfrastructure.py
@@ -1,3 +1,113 @@
+class BatimentDefense():
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idsuivant):
+        self.parent = parent
+        self.id=idsuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+     
+class StationSpatiale(BatimentDefense):
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idsuivant):
+        BatimentDefense.__init__(self, parent, nom, systemeid, planeteid, x, y, idsuivant)
+        self.parent = parent
+        self.id=idsuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+        #======================================================
+        """RESSOURCE"""
+        self.besoinhumain=50
+        self.besoinelectricite= 100
+        self.titanium=1000
+        """STRUCTURE"""
+        self.vie=15000
+        self.dommage=50
+        self.protection=100
+        #======================================================
+    
+    def AugmenterNiveau(self):
+        coutTitanium=10
+        if self.nom.ressource - coutTitanium > 0:
+            pass
+    
+    
+class Mur(BatimentDefense):
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idsuivant):
+        BatimentDefense.__init__(self, parent, nom, systemeid, planeteid, x, y, idsuivant)
+        self.parent = parent
+        self.id=idsuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+        #======================================================
+        """RESSOURCE"""
+        self.bois=300
+        """STRUCTURE"""
+        self.vie= 1000
+        self.protection=100 
+        #======================================================
+
+class Bouclier(BatimentDefense):
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idsuivant):
+        BatimentDefense.__init__(self, parent, nom, systemeid, planeteid, x, y, idsuivant)
+        self.parent=parent
+        self.id=idsuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+        #======================================================
+        """RESSOURCE"""
+        self.titanium=1000
+        self.besoinelectricite=100
+        """STRUCTURE"""
+        self.vie= 10000
+        self.protection=100 
+        self.taille=1000
+        #======================================================
+
+class Tour(BatimentDefense):
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idsuivant):
+        BatimentDefense.__init__(self, parent, nom, systemeid, planeteid, x, y, idsuivant)
+        self.parent = parent
+        self.id=idsuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+        #======================================================
+        """RESSOURCE"""
+        self.bois=300
+        self.besoinhumain=10
+        """STRUCTURE"""
+        self.vie= 1000
+        self.dommage=100
+        self.protection=100
+        #======================================================
+
+class Canon(BatimentDefense):
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idsuivant):
+        BatimentDefense.__init__(self, parent, nom, systemeid, planeteid, x, y, idsuivant)
+        self.parent = parent
+        self.id=idsuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+        #======================================================
+        """RESSOURCE"""
+        self.bois=300
+        self.besoinhumain=10
+        """STRUCTURE"""
+        self.vie= 1000
+        self.dommage=100
+        self.protection=100
+        #======================================================
+        
+        
 class Infrastructure():
     def __init__(self, parent, nom, systemeid, planeteid, x, y, idSuivant):
         self.parent=parent
