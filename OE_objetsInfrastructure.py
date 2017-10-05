2da2dc394b6c869f0ac618a844067c3611b5757e
Guérin Jeffrey <1190931@decinfo.cvm>
2017-10-05 12:27:44 -0400

    version finale Florent et Jeffrey, classe ressources

--- OE_objetsInfrastructure.py
+++ OE_objetsInfrastructure.py
@@ -1,3 +1,84 @@
+import OE_objetsRessource
+
+######Batiment_Ressources#########
+class Batiment_Ressources():
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idSuivant):
+        self.parent=parent
+        self.id=idSuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+        self.CoutRessourcesAmelioration
+"""
+class Mine():
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idSuivant):
+        #self.productionRessource = Ressource()
+        self.multiplicateur = 1.1
+        self.besoinhumain=5
+        self.besoinelectricite=5
+        
+    def detruireMine(self):
+        self.ressource.Humain+self.besoinhumain;
+        self.ressource.Electricite+self.besoinelectricite;
+"""
+######Batiment_Manifacture#########
+class Batiment_Manifacture():
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idSuivant):
+        self.parent=parent
+        self.id=idSuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+
+######Batiment_Infrastructure#########
+class Batiment_Infrastructure():
+    def __init__(self,parent,nom,systemeid,planeteid,x,y,idSuivant):
+        self.parent=parent
+        self.id=idSuivant
+        self.x=x
+        self.y=y
+        self.systemeid=systemeid
+        self.planeteid=planeteid
+"""
+class Ville(Batiment_Infrastructure):
+    def __init__(self,parent,nom, systemeid, planeteid, idSuivant, x=2500,y=2500, proprio="inconnu"):
+        Batiment_Infrastructure.__init__(self, parent, nom, systemeid, planeteid, x, y, idSuivant)
+        self.parent=parent
+        self.id=self.parent.parent.parent.createurId.prochainid()
+        self.x=x
+        self.y=y
+        self.proprietaire=proprio
+        self.taille=20
+        self.type = "Ville"
+               
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class Infrastructure():
     def __init__(self, parent, nom, systemeid, planeteid, x, y, idSuivant):
         self.parent=parent
@@ -28,14 +109,7 @@ class Mine(Infrastructure):
     def detruireMine(self):
         self.ressource.Humain+self.besoinhumain
         self.ressource.Electricite+self.besoinelectricite
-        
-class Ferme(Infrastructure):
-    def __init__(self,parent,nom,systemeid,planeteid,x,y,idSuivant):
-        Infrastructure.__init__(self, parent, nom, systemeid, planeteid, x, y, idSuivant)
-        self.entrepot=0
-        self.besoinhumain=5
-        self.besoinelectricite=5
-        
+    """  
 class Hopital(Infrastructure):
     def __init__(self,parent,nom,systemeid,planeteid,x,y,idSuivant):
         Infrastructure.__init__(self, parent, nom, systemeid, planeteid, x, y, idSuivant)
@@ -44,3 +118,4 @@ class Hopital(Infrastructure):
 class Laboratoire(Infrastructure):
     def __init__(self,parent,nom,systemeid,planeteid,x,y,idSuivant):
         Infrastructure.__init__(self, parent, nom, systemeid, planeteid, x, y, idSuivant)
+"""
