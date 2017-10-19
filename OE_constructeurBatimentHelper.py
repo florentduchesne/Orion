4fdbd3777902f5fd3bb96f37024c9b93fab6b107
Duchesne Florent <1526952@decinfo.cvm>
2017-10-19 11:24:26 -0400

    batiments ressources debut constructeur

---
+++ OE_constructeurBatimentHelper.py
@@ -0,0 +1,9 @@
+import OE_objetsBatiments
+import ListeDesCoutsEtAllocationBatiments
+
+class ConstructeurBatimentHelper():
+    def __init__(self):
+        self.dictionnaire = self.creerDictionnaire()
+    
+    def creerDictionnaire(self):
+        return None
